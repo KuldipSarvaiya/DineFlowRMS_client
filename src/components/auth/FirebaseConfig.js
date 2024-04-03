969a4ac10421c677260b5f14edf587b9970cd8fe
Kuldip Sarvaiya <kuldipsarvaiya94@gmail.com>
2024-04-03 16:13:21 +0530

    auth fixed

--- src/components/auth/FirebaseConfig.js
+++ src/components/auth/FirebaseConfig.js
@@ -5,25 +5,16 @@ import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_API_KEY,
-//   authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-//   projectId: import.meta.env.VITE_PROJECT_ID,
-//   storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-//   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-//   appId: import.meta.env.VITE_APP_ID,
-//   measurementId: import.meta.env.VITE_MEASUREMENT_ID,
-// };
 const firebaseConfig = {
-  apiKey: "AIzaSyD-z7dSIkPeHkxRK6QSBqCiGHa5dp0M5FM",
-  authDomain: "dineflow-8135b.firebaseapp.com",
-  projectId: "dineflow-8135b",
-  storageBucket: "dineflow-8135b.appspot.com",
-  messagingSenderId: "936071356331",
-  appId: "1:936071356331:web:9a1773150efb40d2eafcee",
-  measurementId: "G-7CCZZ0S7MV"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
